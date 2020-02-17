c6347581320c1ba7421e80f562105ce21bd1934e
Ronald Appleton <ronald.appleton@visualsoft.co.uk>
2020-02-17 03:23:48 +0000

    add info boxes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,9 +2,40 @@
 
 @section('content')
 <div class="container">
+    <hr>
     <div class="row">
-
-
+        <div class="col-3 col-md-3">
+            <div class="box box-primary">
+                <div class="box-header" style="font-size: 1.5em;">Active Playlists</div>
+                <div class="box-body">
+                    <p style="font-size: 2em">10</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 col-md-3">
+            <div class="box box-primary">
+                <div class="box-header" style="font-size: 1.5em;">Active Items</div>
+                <div class="box-body">
+                    <p style="font-size: 2em">10</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 col-md-3">
+            <div class="box box-primary">
+                <div class="box-header" style="font-size: 1.5em;">Requests Today</div>
+                <div class="box-body">
+                    <p style="font-size: 2em">10</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-3 col-md-3">
+            <div class="box box-primary">
+                <div class="box-header" style="font-size: 1.5em;">Week Daily AVG</div>
+                <div class="box-body">
+                    <p style="font-size: 2em">10</p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
